6addb027a9ea7c80fdfef5372927dcc75f76b304
SkyxCode <rifkyalkamil@gmail.com>
2025-07-18 16:14:05 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,7 +25,7 @@
         </svg>Download Resume</button> -->
     </div>
     <div class="flex justify-center md:justify-start fadein-right"><img alt="avatar" fetchpriority="high" width="300" height="300" decoding="async" data-nimg="1"
-        class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="https://files.catbox.moe/lhtc95.jpg">
+        class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="https://files.catbox.moe/6ht79m.jpg">
     </div>
   </main>
 </template>
